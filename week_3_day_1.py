770e331e2cd6898d377c47b08e98787ea45ad728
Castro <ddcastro1@cps.k12.il.us>
2024-09-12 14:45:54 -0500

    calculator

--- week_3_day_1.py
+++ week_3_day_1.py
@@ -180,20 +180,33 @@ print(f"{number1},{number2},{number3},{number4},{number5}")
 
 ##########################################################################################
 # #addition
+print(2+1)
 # #multiplication
+print(2*2)
 # #division
+print(6/2)
 # #modulo
+print(17%4)
 # #powers
+print(2**3)
 # #get the max and min of a number
+print(max(1,5))
+print(min(4,5))
 # #round a number
+print(round(3.72837))
 # # absolute value
+print(abs(-3))
 # # order of operations
+print(2*4+3)
 # #to do more you need to import special math libraries from python
-# #from math import *    
+from math import *    
 # #this goes out and grabs some different math functions we can use
 # #floor method
+print(floor(3.7))
 # #ceil method
+print(ceil(3.7))
 # #sqrt method
+print(sqrt(36))
 
 
 
@@ -213,18 +226,33 @@ print(f"{number1},{number2},{number3},{number4},{number5}")
 # # **Getting Input from users**
 # #how do we get input from users?
 # input("what is your name?")
-# # basic math calculator
-# #ask the user for 2 numbers
-# # print out a statement where you:
-# # add them together
-# #multiply
-# # find the max number
-# # find the remainder of the numbers
-# #round one number
+# name = input("What is your name? ")
+# print("Hello, " + name + "!")
+# # # basic math calculator
+# # #ask the user for 2 numbers
+# num1 = int(input("Enter a number. "))
+# num2 = int(input("Enter another number. "))
+# print(num1 + num2)
+# # # print out a statement where you:
+# # # add them together
+# # #multiply
+# print(num1*num2)
+# # # find the max number
+# print(max(num1, num2))
+# # # find the remainder of the numbers
+# print(num1%num2)
+# # #round one number
+# print(round(num1))
 
 
-
-
+print("This is your calculator")
+inp1 = int(input("Please enter your first number. "))
+inp2 = int(input("Please input your second number. "))
+print(inp1+inp2)
+print(inp1-inp2)
+print(inp1*inp2)
+print(inp1%inp2)
+print(inp1/inp2)
 
 
 
